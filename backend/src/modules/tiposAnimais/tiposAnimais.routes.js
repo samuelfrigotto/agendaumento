551229a038a795ad0f6466a9124e795265ac96f9
Samuel <samuelfrigotto@gmail.com>
2026-04-14 12:36:47 -0300

    deploy: 2026-04-14 12:36

--- backend/src/modules/tiposAnimais/tiposAnimais.routes.js
+++ backend/src/modules/tiposAnimais/tiposAnimais.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const tiposAnimaisController = require('./tiposAnimais.controller');
 const { criarValidator, atualizarValidator } = require('./tiposAnimais.validator');
-const auth = require('../../middlewares/auth');
+const { auth } = require('../../middlewares/auth');
 
 // Todas as rotas requerem autenticacao
 router.use(auth);
